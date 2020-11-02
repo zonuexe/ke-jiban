05551ca86d75eab79dcfac855c59c4dda8c56b0a
USAMI Kenta <tadsan@zonu.me>
2020-11-03 03:40:07 +0900

    Add 404

---
+++ pages/404.php
@@ -0,0 +1,5 @@
+<?php http_response_code(404) ?>
+<!DOCTYPE html>
+<title>404 Not Found</title>
+<h1>404 Not Found</h1>
+<p>そんなページゎなぃょ…</p>
--- public/index.php
+++ public/index.php
@@ -7,9 +7,9 @@ require __DIR__ . '/../bootstrap.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = strtoupper($_SERVER['REQUEST_METHOD']);
 
-require __DIR__ . '/../pages/' . [
+require __DIR__ . '/../pages/' . ([
     '/' => [
         'GET' => 'index',
         'POST' => 'index',
     ],
-][$uri][$method] . '.php';
+][$uri][$method] ?? '404') . '.php';
