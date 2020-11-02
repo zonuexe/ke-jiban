16823d12904405ee091f219d2f5a846c6493e7d6
USAMI Kenta <tadsan@zonu.me>
2020-11-03 03:19:39 +0900

    Copy from https://qiita.com/sekaino_usay/items/64fc66523b8239752554

---
+++ public/index.php
@@ -0,0 +1,81 @@
+<html>
+
+<head>
+    <title>掲示板</title>
+</head>
+
+<body>
+
+    <h1>掲示板</h1>
+
+    <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+        <input type="text" name="personal_name" placeholder="名前" required><br><br>
+        <textarea name="contents" rows="8" cols="40" placeholder="投稿内容" required></textarea>
+        <br><br>
+        <input type="submit" name="btn1" value="投稿する">
+    </form>
+
+    <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        writeData();
+    }
+
+          readData();
+
+          function readData(){
+              $keijban_file = 'keijiban.txt';
+
+    $fp = fopen($keijban_file, 'rb');
+
+    if ($fp){
+        if (flock($fp, LOCK_SH)){
+            while (!feof($fp)) {
+                $buffer = fgets($fp);
+                print($buffer);
+            }
+
+            flock($fp, LOCK_UN);
+        }else{
+            print('ファイルロックに失敗しました');
+        }
+    }
+
+    fclose($fp);
+}
+
+function writeData(){
+    $personal_name = $_POST['personal_name'];
+    $contents = $_POST['contents'];
+    $contents = nl2br($contents);
+    $date = date("Y/m/d H:i:s", time()) . "\n";
+    $IP = $_SERVER["REMOTE_ADDR"] ;
+
+    $data = "<hr>";
+    $data = $data."<p>IPアドレス：".$IP."</p>";
+    $data = $data."<p>日時：".$date."</p>";
+    $data = $data."<p>投稿者：".$personal_name."</p>";
+    $data = $data."<p>内容：".$contents."</p>";
+
+    $keijban_file = 'keijiban.txt';
+
+    $fp = fopen($keijban_file, 'ab');
+
+    if ($fp){
+        if (flock($fp, LOCK_EX)){
+            if (fwrite($fp,  $data) === FALSE){
+                print('ファイル書き込みに失敗しました');
+            }
+
+            flock($fp, LOCK_UN);
+        }else{
+            print('ファイルロックに失敗しました');
+        }
+    }
+
+    fclose($fp);
+}
+
+?>
+</body>
+
+</html>
