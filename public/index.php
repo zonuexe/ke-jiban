7e5566fbb56da8c92ee74546accbf3687fa5d9c5
USAMI Kenta <tadsan@zonu.me>
2020-11-03 06:00:07 +0900

    BASIC認証

--- public/index.php
+++ public/index.php
@@ -9,6 +9,14 @@ header('X-Frame-Options: SAMEORIGIN');
 header('X-Robots-Tag: noindex, nofollow');
 header('X-XSS-Protection: 1; mode=block');
 
+[$type, $token] = explode(' ', $_SERVER['HTTP_AUTHORIZATION'] ?? ' ', 2);
+
+if ($type !== 'Basic' || base64_decode($token) !== 'にゃーん:nya-n') {
+    http_response_code(403);
+    header('WWW-Authenticate: Basic realm="Nyaan", charset="UTF-8"');
+    return;
+}
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = strtoupper($_SERVER['REQUEST_METHOD']);
 
